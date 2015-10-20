38dd917a271d0b63c3576e03f3aea39bc442872a
Alexandre Oliveira <rockytvbr@gmail.com>
2015-10-20 14:49:52 -0200

    Initial commit :shipit:

---
+++ EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Duality.Editor;
+
+namespace RockyTV.GitPlugin.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class DualityGitEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "DualityGitEditorPlugin"; }
+		}
+	}
+}
